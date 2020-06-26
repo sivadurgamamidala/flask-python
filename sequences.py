e78a05ea0519dea1e5c24c42f314903202d085a3
sivadurgamamidala <55800155+sivadurgamamidala@users.noreply.github.com>
2020-06-26 11:18:24 +0530

    added sequences.py

---
+++ sequences.py
@@ -0,0 +1,4 @@
+name = "shiva durga mamidala"
+coordinates = (10.0, 20.0)
+names = ["vani","kumari","giri","babu"]
+print(f"{name}'s mother name is {names[0]+names[1]} and his father name is {names[2]+names[3]}. his address coordinates is {coordinates[0]}, {coordinates[1]}")
