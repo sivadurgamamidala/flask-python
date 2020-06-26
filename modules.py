89681bf2a97d833d7323d73112297df8647d1e53
sivadurgamamidala <55800155+sivadurgamamidala@users.noreply.github.com>
2020-06-26 17:27:37 +0530

    added modules.py and cover the concept on modules and importing the definations from functions1.py

---
+++ modules.py
@@ -0,0 +1,7 @@
+from functions1 import square
+from functions1 import main
+from functions1 import main1
+x = int(input("please enter the value that you want to square: "))
+print(square(x))
+# print(main())
+# print(main1())
