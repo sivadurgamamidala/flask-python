df4c815418c13c260ae33fd47450c1306429ff2f
sivadurgamamidala <55800155+sivadurgamamidala@users.noreply.github.com>
2020-06-26 16:34:13 +0530

    added 6 lines of statement

--- dictionaries.py
+++ dictionaries.py
@@ -2,3 +2,9 @@ ages = {"Alice": 22, "Bob": 27}
 ages["charlie"] = 30
 ages["Alice"] += 1
 print(ages)
+x = ages["Alice"]
+y = ages["Bob"]
+z = ages["charlie"]
+print(f"Alice age is {x}")
+print(f"Bob age is {y}")
+print(f"charlie age is {z}")
