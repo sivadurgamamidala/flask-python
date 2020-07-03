f731ce73108434ff8833870e078ba3346a9b695a
sivadurgamamidala <55800155+sivadurgamamidala@users.noreply.github.com>
2020-07-03 10:36:43 +0530

    added few lines of code

--- loop1.py
+++ loop1.py
@@ -1,3 +1,9 @@
 names = ["shiva durga mamidala", "vani kumari mamidala", "giri babu mamidala", "tulasi sai priya mamidala"]
+print("\ndisplaying only string values:")
 for name in names:
     print(name)
+names1 = ["shiva durga mamidala", "vani kumari mamidala", "giri babu mamidala", "tulasi sai priya mamidala",11405042]
+print("\ndisplaying different data types values: ")
+for name in names1:
+    print(name)
+print("\"for a good practice use same data values in arrays\"")
