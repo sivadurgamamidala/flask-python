f9486ed0586f7dca7d8aaa4a6c37d3236c9642f8
sivadurgamamidala <55800155+sivadurgamamidala@users.noreply.github.com>
2020-07-03 10:39:20 +0530

    added shiva age and two lines of code

--- dictionaries.py
+++ dictionaries.py
@@ -1,10 +1,13 @@
-ages = {"Alice": 22, "Bob": 27}
+ages = {"Alice": 22, "Bob": 27, "shiva": 20}
 ages["charlie"] = 30
 ages["Alice"] += 1
+ages["shiva"] += 5
 print(ages)
 x = ages["Alice"]
 y = ages["Bob"]
 z = ages["charlie"]
+a = ages["shiva"]
 print(f"Alice age is {x}")
 print(f"Bob age is {y}")
 print(f"charlie age is {z}")
+print(f"shiva's age is {a}")
