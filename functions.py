be0035948f7590de44dfcfe0123b7d4bbd563a95
sivadurgamamidala <55800155+sivadurgamamidala@users.noreply.github.com>
2020-07-03 10:38:17 +0530

    added four lines of code and that defined power^4 function

--- functions.py
+++ functions.py
@@ -2,9 +2,13 @@ def square(x):
     return x*x
 def cube(y):
     return y*y*y
+def power4(z):
+    return z*z*z*z
 print("Squares of number 0 - 9:")
 for i in range(10):
     print("{} square is {}".format(i, square(i)))
 print("cubes of number 0 - 9:")
 for i in range(10):
     print("{} cube is {}".format(i, cube(i)))
+for i in range(10):
+    print("{} power(4) is {}".format(i, power4(i)))
