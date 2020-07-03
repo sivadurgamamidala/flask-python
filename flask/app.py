72f5184201e262f1efdfc37deb6a72341a524030
sivadurgamamidala <55800155+sivadurgamamidala@users.noreply.github.com>
2020-07-03 16:20:28 +0530

    navigate to different webpages and displaying different messages

--- flask/app.py
+++ flask/app.py
@@ -4,6 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World! this is shiva durga mamidala"
+    return "<h1>Hello World! this is shiva durga mamidala</h1>"
 
-    
+@app.route("/shiva")
+def shiva():
+    return "hello world! this is shiva"
+
+@app.route("/tulasi")
+def tulasi():
+    return "welcome tulasi"
+
+@app.route("/<string:name>")
+def name(name):
+    name = name.capitalize()
+    return f"<h1>Hello, {name}!</h1>"
